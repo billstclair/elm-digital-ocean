8a60ef8abf193a815afbf577c5331ffcf2bc54b9
Bill St. Clair <billstclair@gmail.com>
2017-02-04 18:31:14 -0500

    Restore DigitalOceanAccounts.elm

--- src/DigitalOceanAccounts.elm
+++ src/DigitalOceanAccounts.elm
@@ -1,1 +1,25 @@
-/Users/billstclair/erlang/elm/DigitalOceanAccounts.elm
+----------------------------------------------------------------------
+--
+-- DigitalOceanAccounts.elm
+-- Test accounts.
+-- Copyright (c) 2017 Bill St. Clair <billstclair@gmail.com>
+-- Some rights reserved.
+-- Distributed under the MIT License
+-- See LICENSE.txt
+--
+----------------------------------------------------------------------
+
+module DigitalOceanAccounts exposing ( Account, testAccounts )
+
+import DigitalOcean exposing (AccountInfoResult)
+
+type alias Account =
+    { name : String
+    , token : String
+    , info : Maybe AccountInfoResult
+    }
+
+testAccounts : List Account
+testAccounts =
+    [ -- Add your private accounts here during development.
+    ]
